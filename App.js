e79e5314d8f4f67f021b0bcd7d64a5df9970679f
isfopo <55164207+isfopo@users.noreply.github.com>
2020-05-18 23:26:09 -0400

    impliments basic rotation visualization

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { DeviceMotion } from 'expo-sensors';
+
+import { BarChart } from 'react-native-svg-charts'
 
 export default function App() {
+  const [rotation, setRotation] = useState({});
+
+  useEffect(() => {
+    DeviceMotion.addListener(deviceMotionData => {
+      setRotation(deviceMotionData.rotation);
+      // alpha is for rotation around Z axis, beta for X axis rotation and gamma for Y axis rotation
+    });
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      DeviceMotion.removeAllListeners();
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.sensor}>
+      <BarChart
+        style={{ flex: 1, marginLeft: 8 }}
+        data={[rotation.gamma, rotation.alpha, rotation.beta]}
+        horizontal={true}
+        svg={{ fill: 'rgba(134, 65, 244, 0.8)', }}
+        contentInset={{ top: 10, bottom: 10 }}
+        spacing={0.2}
+        gridMin={0}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+  sensor: {
+    marginTop: 45,
+    paddingHorizontal: 10,
+    flexDirection: 'row', 
+    height: 200, 
+    paddingVertical: 16
+  },
+  text: {
+    textAlign: 'center',
   },
 });
